87ab3d8fda9436fb2fe18c093879131f1d70d662
Max <maximilian.schroder@bi.no>
2021-11-15 13:57:31 +0100

    update to example file

--- example.py
+++ example.py
@@ -1,8 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 15 13:26:30 2021
-
-@author: A2010183
-"""
-
+# Import the SDW_API class
 from sdw_api import SDW_API
+
+# define a list of tickers. In this case Inflation, GDP, Eonia, and employment in
+# hour worked
+tickers = ['ICP.M.U2.Y.XEF000.3.INX',
+           'MNA.Q.Y.I8.W2.S1.S1.B.B1GQ._Z._Z._Z.EUR.LR.N',
+           'FM.M.U2.EUR.4F.MM.EONIA.HSTA',
+           'ENA.Q.Y.I8.W2.S1.S1._Z.EMP._Z._T._Z.HW._Z.N']
+
+# initialize the class
+example = SDW_API(tickers)
+
+# call the class to download the data
+example()
+
+# he data file can be retrieved with
+example.data
